aa88994b46c1ba40effb931fa96ab8a6fe321dc1
Vaibhav Dubey <121270051+vaibhavvdubeyy@users.noreply.github.com>
2023-02-13 12:32:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 import React, { Component } from 'react';
 import Navbar from './Component/Navbar';
 import News from './Component/News';
@@ -8,10 +8,14 @@ import News from './Component/News';
 export class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <Navbar/>
-        <News/>
-      </div>
+        <Routes>
+           
+           <Route path="/" element={<News />}/>
+        </Routes>
+        
+      </Router>
     );
   }
 }
